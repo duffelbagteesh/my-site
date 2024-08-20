163d8c0d22741f21f2fe31546fde99c0a0d61941
duffelbagteesh <leticia.tishy@gmail.com>
2024-08-20 18:08:18 -0400

    p5 fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,11 +7,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap" />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
           {/* Add other global stylesheets here */}
         </Head>
         <body>
           <Main />
+          <NextScript />
         </body>
       </Html>
     )
